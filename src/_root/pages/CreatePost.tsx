d1b0844fda29fa5244a4b9420397dd304c0f9374
matheusz nied <matheustae@hotmail.com>
2024-01-07 18:33:07 -0300

    feat: Create Post form

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import PostForm from "@/components/forms/PostForm";
+import React from "react";
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
-  )
-}
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="max-w-5xl flex-start gap-3 justify-start w-full">
+          <img
+            src="/assets/icons/add-post.svg"
+            alt="add post"
+            width={36}
+            height={36}
+          />
+          <h2 className="h3-bold md:h2-bold text-left w-full">Create Post</h2>
+        </div>
 
-export default CreatePost
+        <PostForm/>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePost;
